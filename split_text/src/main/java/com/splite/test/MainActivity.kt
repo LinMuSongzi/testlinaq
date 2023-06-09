2b5c7c2c9e2806f5577bd099e74ba14e5bd75901
林辉 <536451207@qq.com>
2023-06-09 15:36:43 +0800

    test1

--- split_text/src/main/java/com/splite/test/MainActivity.kt
+++ split_text/src/main/java/com/splite/test/MainActivity.kt
@@ -1,35 +1,18 @@
 package com.splite.test
 
-import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.splite.test.databinding.ActivityMainBinding
+import android.os.Bundle
+import com.splite.test.Comment.start
+import com.splite.test.databinding.ActivityFifthBinding
+import com.splite.test.databinding.ActivityFourthBinding
+import com.splite.test.databinding.ActivityFristBinding
+import com.splite.test.databinding.ActivityNinthBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : DataBindingActivity<ActivityFristBinding>() {
 
-    private lateinit var binding: ActivityMainBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+    override fun onCreate2(savedInstanceState: Bundle?) {
+        dataBinding.frist.setOnClickListener {
+            SecondActivity::class.java.start(this)
+        }
     }
 }
--- split_text/src/main/java/com/splite/test/SplitInitializer.kt
+++ split_text/src/main/java/com/splite/test/SplitInitializer.kt
@@ -2,17 +2,17 @@ package com.splite.test
 
 import android.content.Context
 import androidx.startup.Initializer
-import androidx.window.embedding.RuleController
-
-class SplitInitializer : Initializer<RuleController> {
-
-    override fun create(context: Context): RuleController {
-        return RuleController.getInstance(context).apply {
-            setRules(RuleController.parseRules(context, R.xml.main_split_config))
-        }
-    }
-
-    override fun dependencies(): List<Class<out Initializer<*>>> {
-        return emptyList()
-    }
-}
+//import androidx.window.embedding.RuleController
+//
+//class SplitInitializer : Initializer<RuleController> {
+//
+//    override fun create(context: Context): RuleController {
+//        return RuleController.getInstance(context).apply {
+//            setRules(RuleController.parseRules(context, R.xml.main_split_config))
+//        }
+//    }
+//
+//    override fun dependencies(): List<Class<out Initializer<*>>> {
+//        return emptyList()
+//    }
+//}
